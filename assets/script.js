bf05e0cb5ae3c8e391c7df41b0d7ec2729345e66
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-27 20:32:56 +0000

    format script.js file

--- assets/script.js
+++ assets/script.js
@@ -29,7 +29,7 @@ function clear() {
   $("#article-section").empty();
 }
 
-$("#run-search").on("click", event => {
+$("#run-search").on("click", (event) => {
   // prevent the page from refreshing when search button is pressed
   event.preventDefault();
 
@@ -37,40 +37,45 @@ $("#run-search").on("click", event => {
   clear();
 
   var queryURL = createQueryURL();
-  console.log(queryURL)
+  console.log(queryURL);
 
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(data => {
-
+  }).then((data) => {
     console.log(data);
     var numOfArticles = $("#article-count").val();
-    console.log(numOfArticles)
+    console.log(numOfArticles);
 
     for (var i = 0; i < numOfArticles; i++) {
-        
-        var indivArticle = data.response.docs[i];
+      var indivArticle = data.response.docs[i];
 
-        var articleOList = $('<ol>');
-        articleOList.addClass('list-group');
+      var articleOList = $("<ol>");
+      articleOList.addClass("list-group");
 
-        $("#article-section").append(articleOList);
+      $("#article-section").append(articleOList);
 
-        var listEl = $("<li class='list-group-item list-group-item-action headline'> ")
-        listEl.append("<h2>" + indivArticle.headline.main + "</h2>")
-        listEl.append("<h3>" + indivArticle.byline.original + "</h3>");
-        listEl.append("<h5>Section: " + indivArticle.section_name + "</h5>");
+      var listEl = $(
+        "<li class='list-group-item list-group-item-action headline'> "
+      );
+      listEl.append("<h2>" + indivArticle.headline.main + "</h2>");
+      listEl.append("<h3>" + indivArticle.byline.original + "</h3>");
+      listEl.append("<h5>Section: " + indivArticle.section_name + "</h5>");
 
-        var dates = indivArticle.pub_date.split("T")
-        var pubDateAndTime = dates[0] + " " + dates[1].slice(0, -5);
-        
-        listEl.append("<h5>" + pubDateAndTime + "</h5>");
-        listEl.append("<a href='" + indivArticle.web_url + "'>" + indivArticle.web_url + "</a>");
+      var dates = indivArticle.pub_date.split("T");
+      var pubDateAndTime = dates[0] + " " + dates[1].slice(0, -5);
 
-        articleOList.append(listEl)
+      listEl.append("<h5>" + pubDateAndTime + "</h5>");
+      listEl.append(
+        "<a href='" +
+          indivArticle.web_url +
+          "'>" +
+          indivArticle.web_url +
+          "</a>"
+      );
+
+      articleOList.append(listEl);
     }
-
   });
 });
 
